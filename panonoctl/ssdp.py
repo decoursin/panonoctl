54ea10840f73708cbe14f6cdc043d3c4e4ea466a
Lehner Florian <dev@der-flo.net>
2016-04-10 21:18:55 +0200

    Code clean up
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- panonoctl/ssdp.py
+++ panonoctl/ssdp.py
@@ -17,22 +17,33 @@ import socket
 class ssdpNotify:
     @staticmethod
     def discover(self):
-        print "discover"
+        req =  ('M-SEARCH * HTTP/1.1\r\n' +
+                'MX: 10\r\n' +
+                'HOST: 239.255.255.250:1900\r\n' +
+                'MAN: \"ssdp:discover\"\r\n' +
+                'NT: panono:ball-camera\r\n' +
+                '\r\n')
         ip = None
         port = None
         path = None
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(7)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.settimeout(5)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('',1900))
-        sock.sendto("M-SEARCH * HTTP/1.1\r\nMX: 10\r\nHOST: 239.255.255.250:1900\r\nMAN: \"ssdp:discover\"\r\nNT: panono:ball-camera\r\n\r\n", ('239.255.255.250', 1900))
-        while True:
-            print "."
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        sock.bind(('', 1900))
+        for _ in range(5):
             try:
-                data, addr = sock.recvfrom(1024)#.decode('utf-8')
-                print data
+                sock.sendto( req, ('239.255.255.250', 1900))
             except socket.error as e:
                 print e
-                break
+                return (None, None, None)
+            while True:
+                try:
+                    data = sock.recv(1024)#.decode('utf-8')
+                    if not data: continue
+                    print data
+                except socket.error as e:
+                    print e
+                    break
         sock.close()
         return (ip, port, path)
