6e346a7e062e5b76b4d252617a6b59ed24341032
Lehner Florian <dev@der-flo.net>
2016-05-11 17:45:06 +0200

    Version 0.4.1
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.md') as f:
 
 setup(
     name='panonoctl',
-    version='0.4',
+    version='0.4.1',
     py_modules=['panonoctl'],
     packages=find_packages(),
     long_description=readme,
